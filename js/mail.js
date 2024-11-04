6e47f335242260558966971b4f489e4ae9e076db
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-04 14:25:29 +0100

    feat:Mail check completed

--- js/mail.js
+++ js/mail.js
@@ -4,13 +4,17 @@ const inpMail = prompt("Inserisci la tua e-mail");
 // Lista mail che possono accedere
 const listaMail = ["bob43@gmail.com", "billy92@gmail.com", "fra342@gmail.com"];
 
+let isThere = false;
+
 // Controllo se è nella lista di chi può accedere
 for (let i = 0; i < listaMail.length; i++) {
   const currString = listaMail[i];
 
-  // Se la mail è nell'array fermo il ciclo e faccio un console.log
+  // Se la mail è nell'array e isThere è falso
   if (currString === inpMail) {
-    console.log("La mail inserita è nella lista");
-    break;
+    isThere = true;
   }
 }
+
+if (isThere) console.log(`La mail ${inpMail} è nella lista`);
+else console.log(`La mail ${inpMail} non è nella lista`);
